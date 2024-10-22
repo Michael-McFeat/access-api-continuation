1b4538402ff108aa1402461879f6a3953b58154b
James Mboma <james.mboma@fisglobal.com>
2024-10-22 10:46:27 +0100

    fixed cors errors

--- src/components/Payments.jsx
+++ src/components/Payments.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useLayoutEffect, useState} from "react";
 import "./Payments.css";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuidv4} from 'uuid';
 
 
 // function scriptAlreadyLoaded(src) {
@@ -75,6 +76,9 @@ import { useSelector, useDispatch } from "react-redux";
 export function Payments() {
 
   const value = useSelector((state) => state.cart.total);
+  const credentials = btoa("WULlOqbjOuxGKpC4" + ":" + "Gxxm9GQZKm5WeboUmUmUaHblIHNH71b4UKrT9z0AMIkp2MEZgVnFAkcEvAWNB0si");
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const targetUrl = 'https://try.access.worldpay.com/api/payments';
 
   const [formData, setFormData] = useState({
     fullName: "",
@@ -97,189 +101,190 @@ export function Payments() {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
 
-    const testData =
-    {
-      "transactionReference": "Memory265-13/08/1876",
-        "merchant": {
-      "entity": "default"
-    },
-      "instruction": {
-      "method": "card",
-          "paymentInstrument": {
-        "type": "plain",
-            "cardHolderName": "Sherlock Holmes",
-            "cardNumber": formData.cardNumber,
-            "expiryDate": {
-          "month": parseInt(formData.expMonth),
-              "year": parseInt(formData.expYear)
-        },
-        "billingAddress": {
-          "address1": "221B Baker Street",
-              "address2": "Marylebone",
-              "address3": "Westminster",
-              "postalCode": "SW1 1AA",
-              "city": "London",
-              "state": "Greater London",
-              "countryCode": "GB"
-        },
-        "cvc": "123"
-      },
-      "narrative": {
-        "line1": "trading name"
-      },
-      "value": {
-        "currency": "GBP",
-            "amount": parseFloat(value)
-      }
+      const testData =
+          {
+            "transactionReference": `${uuidv4()}${Date.now()}`,
+            "merchant": {
+              "entity": "default"
+            },
+            "instruction": {
+              "method": "card",
+              "paymentInstrument": {
+                "type": "plain",
+                "cardHolderName": formData.cardName,
+                "cardNumber": formData.cardNumber,
+                "expiryDate": {
+                  "month": parseInt(formData.expMonth),
+                  "year": parseInt(formData.expYear)
+                },
+                "billingAddress": {
+                  "address1": "221B Baker Street",
+                  "address2": "Marylebone",
+                  "address3": "Westminster",
+                  "postalCode": "SW1 1AA",
+                  "city": "London",
+                  "state": "Greater London",
+                  "countryCode": "GB"
+                },
+                "cvc": "123"
+              },
+              "narrative": {
+                "line1": "trading name"
+              },
+              "value": {
+                "currency": "GBP",
+                "amount": parseFloat(value)
+              }
+            }
+
+          }
+
+      console.log(testData);
+
+
+     const res= await axios.post(proxyUrl + targetUrl, testData, {
+        headers: {
+          'Authorization': `Basic ${credentials}`,
+          'Content-Type': 'application/json',
+          'WP-Api-Version': '2024-06-01'
+        }
+      });
+
+      console.log(res);
+    } catch (e) {
+      console.log(e)
     }
-
-    }
-
-    console.log(testData);
-
-    axios.post('https://try.access.worldpay.com/api/payments', testData, {
-      auth: {
-        username: 'WULlOqbjOuxGKpC4',
-        password: ' Gxxm9GQZKm5WeboUmUmUaHblIHNH71b4UKrT9z0AMIkp2MEZgVnFAkcEvAWNB0si'
-      },
-      headers: {
-        'content-type': 'application/json',
-        'WP-Api-Version': '2024-06-01'
-    }}).then(response => {
-      console.log(response);
-    });
-
-    //console.log(formData);
   }
 
-  return (
-      <div className="row">
-        <div className="col-75">
-          <div className="container">
-            <form onSubmit={handleSubmit}>
-              <div className="row">
-                <div className="col-50">
-                  <h3>Billing Address</h3>
-                  <label htmlFor="fullName">Full Name</label>
-                  <input
-                      type="text"
-                      id="fullName"
-                      name="fullName"
-                      value={formData.fullName}
-                      onChange={handleChange}
-                      placeholder="John M. Doe"
-                  />
-                  <label htmlFor="address">Address</label>
-                  <input
-                      type="text"
-                      id="address"
-                      name="address"
-                      value={formData.address}
-                      onChange={handleChange}
-                      placeholder="542 W. 15th Street"
-                  />
-                  <label htmlFor="city">City</label>
-                  <input
-                      type="text"
-                      id="city"
-                      name="city"
-                      value={formData.city}
-                      onChange={handleChange}
-                      placeholder="New York"
-                  />
+    return (
+        <div className="row">
+          <div className="col-75">
+            <div className="container">
+              <form onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-50">
+                    <h3>Billing Address</h3>
+                    <label htmlFor="fullName">Full Name</label>
+                    <input
+                        type="text"
+                        id="fullName"
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                        placeholder="John M. Doe"
+                    />
+                    <label htmlFor="address">Address</label>
+                    <input
+                        type="text"
+                        id="address"
+                        name="address"
+                        value={formData.address}
+                        onChange={handleChange}
+                        placeholder="542 W. 15th Street"
+                    />
+                    <label htmlFor="city">City</label>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        value={formData.city}
+                        onChange={handleChange}
+                        placeholder="New York"
+                    />
 
-                  <div className="row">
-                    <div className="col-50">
-                      <label htmlFor="state">State</label>
-                      <input
-                          type="text"
-                          id="state"
-                          name="state"
-                          value={formData.state}
-                          onChange={handleChange}
-                          placeholder="NY"
-                      />
+                    <div className="row">
+                      <div className="col-50">
+                        <label htmlFor="state">State</label>
+                        <input
+                            type="text"
+                            id="state"
+                            name="state"
+                            value={formData.state}
+                            onChange={handleChange}
+                            placeholder="NY"
+                        />
+                      </div>
+                      <div className="col-50">
+                        <label htmlFor="postalCode">Postal Code</label>
+                        <input
+                            type="text"
+                            id="postalCode"
+                            name="postalCode"
+                            value={formData.postalCode}
+                            onChange={handleChange}
+                            placeholder="10001"
+                        />
+                      </div>
                     </div>
-                    <div className="col-50">
-                      <label htmlFor="postalCode">Postal Code</label>
-                      <input
-                          type="text"
-                          id="postalCode"
-                          name="postalCode"
-                          value={formData.postalCode}
-                          onChange={handleChange}
-                          placeholder="10001"
-                      />
+                  </div>
+
+                  <div className="col-50">
+                    <h3>Payment</h3>
+                    <label htmlFor="cardName">Name on Card</label>
+                    <input
+                        type="text"
+                        id="cardName"
+                        name="cardName"
+                        value={formData.cardName}
+                        onChange={handleChange}
+                        placeholder="John More Doe"
+                    />
+                    <label htmlFor="cardNumber">Credit card number</label>
+                    <input
+                        type="text"
+                        id="cardNumber"
+                        name="cardNumber"
+                        value={formData.cardNumber}
+                        onChange={handleChange}
+                        placeholder="1111-2222-3333-4444"
+                    />
+                    <label htmlFor="expMonth">Exp Month</label>
+                    <input
+                        type="text"
+                        id="expMonth"
+                        name="expMonth"
+                        value={formData.expMonth}
+                        onChange={handleChange}
+                        placeholder="September"
+                    />
+                    <div className="row">
+                      <div className="col-50">
+                        <label htmlFor="expYear">Exp Year</label>
+                        <input
+                            type="text"
+                            id="expYear"
+                            name="expYear"
+                            value={formData.expYear}
+                            onChange={handleChange}
+                            placeholder="2023"
+                        />
+                      </div>
+                      <div className="col-50">
+                        <label htmlFor="cvv">CVV</label>
+                        <input
+                            type="text"
+                            id="cvv"
+                            name="cvv"
+                            value={formData.cvv}
+                            onChange={handleChange}
+                            placeholder="352"
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
-
-                <div className="col-50">
-                  <h3>Payment</h3>
-                  <label htmlFor="cardName">Name on Card</label>
-                  <input
-                      type="text"
-                      id="cardName"
-                      name="cardName"
-                      value={formData.cardName}
-                      onChange={handleChange}
-                      placeholder="John More Doe"
-                  />
-                  <label htmlFor="cardNumber">Credit card number</label>
-                  <input
-                      type="text"
-                      id="cardNumber"
-                      name="cardNumber"
-                      value={formData.cardNumber}
-                      onChange={handleChange}
-                      placeholder="1111-2222-3333-4444"
-                  />
-                  <label htmlFor="expMonth">Exp Month</label>
-                  <input
-                      type="text"
-                      id="expMonth"
-                      name="expMonth"
-                      value={formData.expMonth}
-                      onChange={handleChange}
-                      placeholder="September"
-                  />
-                  <div className="row">
-                    <div className="col-50">
-                      <label htmlFor="expYear">Exp Year</label>
-                      <input
-                          type="text"
-                          id="expYear"
-                          name="expYear"
-                          value={formData.expYear}
-                          onChange={handleChange}
-                          placeholder="2023"
-                      />
-                    </div>
-                    <div className="col-50">
-                      <label htmlFor="cvv">CVV</label>
-                      <input
-                          type="text"
-                          id="cvv"
-                          name="cvv"
-                          value={formData.cvv}
-                          onChange={handleChange}
-                          placeholder="352"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <label>
-                <input type="checkbox" name="sameAddress" /> Shipping address same as billing
-              </label>
-              <input type="submit" value="Continue to checkout" className="btn" />
-            </form>
+                <label>
+                  <input type="checkbox" name="sameAddress"/> Shipping address same as billing
+                </label>
+                <input type="submit" value="Continue to checkout" className="btn"/>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-  );
-}
+    );
+  }
 
